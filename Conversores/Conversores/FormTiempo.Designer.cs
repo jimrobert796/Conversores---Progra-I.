74cb8c3d9a46aac82643d2883137e07e7455853e
jimrobert796 <jimmyroberto270@gmail.com>
2025-08-19 14:51:33 -0600

    Formulario Tiempo hecho, se cambio el nombre de los forms a FormMoneda y FormTiempo

---
+++ Conversores/Conversores/FormTiempo.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace Conversores
+{
+    partial class FormConverTiemp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNanosegundos = new System.Windows.Forms.Label();
+            this.lblMicrosegundos = new System.Windows.Forms.Label();
+            this.lblMilisegundos = new System.Windows.Forms.Label();
+            this.lblSegundos = new System.Windows.Forms.Label();
+            this.lblMinutos = new System.Windows.Forms.Label();
+            this.lblHoras = new System.Windows.Forms.Label();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.lblSemanas = new System.Windows.Forms.Label();
+            this.lblMeses = new System.Windows.Forms.Label();
+            this.lblNombreConvertidorMonedas = new System.Windows.Forms.Label();
+            this.lblAños = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbNanosegundos = new System.Windows.Forms.RadioButton();
+            this.rbMicrosegundos = new System.Windows.Forms.RadioButton();
+            this.rbMilisegundos = new System.Windows.Forms.RadioButton();
+            this.rbSegundos = new System.Windows.Forms.RadioButton();
+            this.rbMinutos = new System.Windows.Forms.RadioButton();
+            this.rbHoras = new System.Windows.Forms.RadioButton();
+            this.rbDias = new System.Windows.Forms.RadioButton();
+            this.rbSemanas = new System.Windows.Forms.RadioButton();
+            this.rbMeses = new System.Windows.Forms.RadioButton();
+            this.rbAños = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNanosegundos
+            // 
+            this.lblNanosegundos.AutoSize = true;
+            this.lblNanosegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNanosegundos.Location = new System.Drawing.Point(9, 387);
+            this.lblNanosegundos.Name = "lblNanosegundos";
+            this.lblNanosegundos.Size = new System.Drawing.Size(128, 16);
+            this.lblNanosegundos.TabIndex = 41;
+            this.lblNanosegundos.Text = "Nanosegundos: ?";
+            // 
+            // lblMicrosegundos
+            // 
+            this.lblMicrosegundos.AutoSize = true;
+            this.lblMicrosegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMicrosegundos.Location = new System.Drawing.Point(9, 362);
+            this.lblMicrosegundos.Name = "lblMicrosegundos";
+            this.lblMicrosegundos.Size = new System.Drawing.Size(129, 16);
+            this.lblMicrosegundos.TabIndex = 42;
+            this.lblMicrosegundos.Text = "Microsegundos: ?";
+            // 
+            // lblMilisegundos
+            // 
+            this.lblMilisegundos.AutoSize = true;
+            this.lblMilisegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMilisegundos.Location = new System.Drawing.Point(9, 336);
+            this.lblMilisegundos.Name = "lblMilisegundos";
+            this.lblMilisegundos.Size = new System.Drawing.Size(115, 16);
+            this.lblMilisegundos.TabIndex = 43;
+            this.lblMilisegundos.Text = "Milisegundos: ?";
+            // 
+            // lblSegundos
+            // 
+            this.lblSegundos.AutoSize = true;
+            this.lblSegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundos.Location = new System.Drawing.Point(9, 311);
+            this.lblSegundos.Name = "lblSegundos";
+            this.lblSegundos.Size = new System.Drawing.Size(115, 16);
+            this.lblSegundos.TabIndex = 44;
+            this.lblSegundos.Text = "Segundos (s): ?";
+            // 
+            // lblMinutos
+            // 
+            this.lblMinutos.AutoSize = true;
+            this.lblMinutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutos.Location = new System.Drawing.Point(9, 282);
+            this.lblMinutos.Name = "lblMinutos";
+            this.lblMinutos.Size = new System.Drawing.Size(76, 16);
+            this.lblMinutos.TabIndex = 45;
+            this.lblMinutos.Text = "Minutos: ?";
+            // 
+            // lblHoras
+            // 
+            this.lblHoras.AutoSize = true;
+            this.lblHoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoras.Location = new System.Drawing.Point(9, 254);
+            this.lblHoras.Name = "lblHoras";
+            this.lblHoras.Size = new System.Drawing.Size(65, 16);
+            this.lblHoras.TabIndex = 46;
+            this.lblHoras.Text = "Horas: ?";
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDias.Location = new System.Drawing.Point(9, 226);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(55, 16);
+            this.lblDias.TabIndex = 40;
+            this.lblDias.Text = "Dias: ?";
+            // 
+            // lblSemanas
+            // 
+            this.lblSemanas.AutoSize = true;
+            this.lblSemanas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSemanas.Location = new System.Drawing.Point(9, 196);
+            this.lblSemanas.Name = "lblSemanas";
+            this.lblSemanas.Size = new System.Drawing.Size(88, 16);
+            this.lblSemanas.TabIndex = 39;
+            this.lblSemanas.Text = "Semanas: ?";
+            // 
+            // lblMeses
+            // 
+            this.lblMeses.AutoSize = true;
+            this.lblMeses.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMeses.Location = new System.Drawing.Point(9, 167);
+            this.lblMeses.Name = "lblMeses";
+            this.lblMeses.Size = new System.Drawing.Size(65, 16);
+            this.lblMeses.TabIndex = 38;
+            this.lblMeses.Text = "Meses ?";
+            // 
+            // lblNombreConvertidorMonedas
+            // 
+            this.lblNombreConvertidorMonedas.AutoSize = true;
+            this.lblNombreConvertidorMonedas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreConvertidorMonedas.Location = new System.Drawing.Point(199, 8);
+            this.lblNombreConvertidorMonedas.Name = "lblNombreConvertidorMonedas";
+            this.lblNombreConvertidorMonedas.Size = new System.Drawing.Size(217, 24);
+            this.lblNombreConvertidorMonedas.TabIndex = 37;
+            this.lblNombreConvertidorMonedas.Text = "Convertidor de tiempo";
+            // 
+            // lblAños
+            // 
+            this.lblAños.AutoSize = true;
+            this.lblAños.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAños.Location = new System.Drawing.Point(9, 137);
+            this.lblAños.Name = "lblAños";
+            this.lblAños.Size = new System.Drawing.Size(58, 16);
+            this.lblAños.TabIndex = 35;
+            this.lblAños.Text = "Años: ?";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(502, 93);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(139, 72);
+            this.btnConvertir.TabIndex = 34;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(159, 93);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(138, 20);
+            this.txtNum1.TabIndex = 33;
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(9, 97);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(144, 16);
+            this.lblNum1.TabIndex = 32;
+            this.lblNum1.Text = "Ingrese la cantidad:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbNanosegundos);
+            this.groupBox1.Controls.Add(this.rbMicrosegundos);
+            this.groupBox1.Controls.Add(this.rbMilisegundos);
+            this.groupBox1.Controls.Add(this.rbSegundos);
+            this.groupBox1.Controls.Add(this.rbMinutos);
+            this.groupBox1.Controls.Add(this.rbHoras);
+            this.groupBox1.Controls.Add(this.rbDias);
+            this.groupBox1.Controls.Add(this.rbSemanas);
+            this.groupBox1.Controls.Add(this.rbMeses);
+            this.groupBox1.Controls.Add(this.rbAños);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(346, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(143, 259);
+            this.groupBox1.TabIndex = 47;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tiempo origen:";
+            // 
+            // rbNanosegundos
+            // 
+            this.rbNanosegundos.AutoSize = true;
+            this.rbNanosegundos.Location = new System.Drawing.Point(7, 230);
+            this.rbNanosegundos.Name = "rbNanosegundos";
+            this.rbNanosegundos.Size = new System.Drawing.Size(109, 19);
+            this.rbNanosegundos.TabIndex = 9;
+            this.rbNanosegundos.Text = "Nanosegundos";
+            this.rbNanosegundos.UseVisualStyleBackColor = true;
+            // 
+            // rbMicrosegundos
+            // 
+            this.rbMicrosegundos.AutoSize = true;
+            this.rbMicrosegundos.Location = new System.Drawing.Point(7, 207);
+            this.rbMicrosegundos.Name = "rbMicrosegundos";
+            this.rbMicrosegundos.Size = new System.Drawing.Size(110, 19);
+            this.rbMicrosegundos.TabIndex = 8;
+            this.rbMicrosegundos.Text = "Microsegundos";
+            this.rbMicrosegundos.UseVisualStyleBackColor = true;
+            // 
+            // rbMilisegundos
+            // 
+            this.rbMilisegundos.AutoSize = true;
+            this.rbMilisegundos.Location = new System.Drawing.Point(7, 184);
+            this.rbMilisegundos.Name = "rbMilisegundos";
+            this.rbMilisegundos.Size = new System.Drawing.Size(99, 19);
+            this.rbMilisegundos.TabIndex = 7;
+            this.rbMilisegundos.Text = "Milisegundos";
+            this.rbMilisegundos.UseVisualStyleBackColor = true;
+            // 
+            // rbSegundos
+            // 
+            this.rbSegundos.AutoSize = true;
+            this.rbSegundos.Location = new System.Drawing.Point(7, 161);
+            this.rbSegundos.Name = "rbSegundos";
+            this.rbSegundos.Size = new System.Drawing.Size(81, 19);
+            this.rbSegundos.TabIndex = 6;
+            this.rbSegundos.Text = "Segundos";
+            this.rbSegundos.UseVisualStyleBackColor = true;
+            // 
+            // rbMinutos
+            // 
+            this.rbMinutos.AutoSize = true;
+            this.rbMinutos.Location = new System.Drawing.Point(7, 136);
+            this.rbMinutos.Name = "rbMinutos";
+            this.rbMinutos.Size = new System.Drawing.Size(69, 19);
+            this.rbMinutos.TabIndex = 5;
+            this.rbMinutos.Text = "Minutos";
+            this.rbMinutos.UseVisualStyleBackColor = true;
+            // 
+            // rbHoras
+            // 
+            this.rbHoras.AutoSize = true;
+            this.rbHoras.Location = new System.Drawing.Point(7, 113);
+            this.rbHoras.Name = "rbHoras";
+            this.rbHoras.Size = new System.Drawing.Size(58, 19);
+            this.rbHoras.TabIndex = 4;
+            this.rbHoras.Text = "Horas";
+            this.rbHoras.UseVisualStyleBackColor = true;
+            // 
+            // rbDias
+            // 
+            this.rbDias.AutoSize = true;
+            this.rbDias.Location = new System.Drawing.Point(7, 90);
+            this.rbDias.Name = "rbDias";
+            this.rbDias.Size = new System.Drawing.Size(50, 19);
+            this.rbDias.TabIndex = 3;
+            this.rbDias.Text = "Dias";
+            this.rbDias.UseVisualStyleBackColor = true;
+            // 
+            // rbSemanas
+            // 
+            this.rbSemanas.AutoSize = true;
+            this.rbSemanas.Location = new System.Drawing.Point(7, 67);
+            this.rbSemanas.Name = "rbSemanas";
+            this.rbSemanas.Size = new System.Drawing.Size(78, 19);
+            this.rbSemanas.TabIndex = 2;
+            this.rbSemanas.Text = "Semanas";
+            this.rbSemanas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.rbSemanas.UseVisualStyleBackColor = true;
+            // 
+            // rbMeses
+            // 
+            this.rbMeses.AutoSize = true;
+            this.rbMeses.Location = new System.Drawing.Point(7, 44);
+            this.rbMeses.Name = "rbMeses";
+            this.rbMeses.Size = new System.Drawing.Size(62, 19);
+            this.rbMeses.TabIndex = 1;
+            this.rbMeses.Text = "Meses";
+            this.rbMeses.UseVisualStyleBackColor = true;
+            // 
+            // rbAños
+            // 
+            this.rbAños.AutoSize = true;
+            this.rbAños.Checked = true;
+            this.rbAños.Location = new System.Drawing.Point(7, 20);
+            this.rbAños.Name = "rbAños";
+            this.rbAños.Size = new System.Drawing.Size(52, 19);
+            this.rbAños.TabIndex = 0;
+            this.rbAños.TabStop = true;
+            this.rbAños.Text = "Años";
+            this.rbAños.UseVisualStyleBackColor = true;
+            // 
+            // FormConverTiemp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 410);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblNanosegundos);
+            this.Controls.Add(this.lblMicrosegundos);
+            this.Controls.Add(this.lblMilisegundos);
+            this.Controls.Add(this.lblSegundos);
+            this.Controls.Add(this.lblMinutos);
+            this.Controls.Add(this.lblHoras);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.lblSemanas);
+            this.Controls.Add(this.lblMeses);
+            this.Controls.Add(this.lblNombreConvertidorMonedas);
+            this.Controls.Add(this.lblAños);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum1);
+            this.Name = "FormConverTiemp";
+            this.Text = "Convertidor de Tiempo";
+            this.Load += new System.EventHandler(this.FormConverTiemp_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNanosegundos;
+        private System.Windows.Forms.Label lblMicrosegundos;
+        private System.Windows.Forms.Label lblMilisegundos;
+        private System.Windows.Forms.Label lblSegundos;
+        private System.Windows.Forms.Label lblMinutos;
+        private System.Windows.Forms.Label lblHoras;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.Label lblSemanas;
+        private System.Windows.Forms.Label lblMeses;
+        private System.Windows.Forms.Label lblNombreConvertidorMonedas;
+        private System.Windows.Forms.Label lblAños;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbNanosegundos;
+        private System.Windows.Forms.RadioButton rbMicrosegundos;
+        private System.Windows.Forms.RadioButton rbMilisegundos;
+        private System.Windows.Forms.RadioButton rbSegundos;
+        private System.Windows.Forms.RadioButton rbMinutos;
+        private System.Windows.Forms.RadioButton rbHoras;
+        private System.Windows.Forms.RadioButton rbDias;
+        private System.Windows.Forms.RadioButton rbSemanas;
+        private System.Windows.Forms.RadioButton rbMeses;
+        private System.Windows.Forms.RadioButton rbAños;
+    }
+}
--- Conversores/Conversores/Program.cs
+++ Conversores/Conversores/Program.cs
@@ -16,7 +16,11 @@ namespace Conversores
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormConverMoned());
+            // Iniciar las dos formas
+            // aqui solo incia el 2 en este caso para su funcionalidad
+            //Application.Run(new FormConverMoned());
+
+            Application.Run(new FormConverTiemp());
         }
     }
 }
