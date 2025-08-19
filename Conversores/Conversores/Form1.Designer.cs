d5b3fade7f2eef8585385ad3a0fb82fe303469d4
jimrobert796 <jimmyroberto270@gmail.com>
2025-08-18 22:50:38 -0600

    Se agrego el formulario de conversion de monedas con un Combobox

---
+++ Conversores/Conversores/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Conversores
+{
+    partial class FormConverMoned
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuetzal = new System.Windows.Forms.Label();
+            this.lblPesoMexicano = new System.Windows.Forms.Label();
+            this.lblSolPeruano = new System.Windows.Forms.Label();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.lblLempiraHondureño = new System.Windows.Forms.Label();
+            this.lblDolarEstadounidense = new System.Windows.Forms.Label();
+            this.lblDolarCanadiense = new System.Windows.Forms.Label();
+            this.lblCordoba = new System.Windows.Forms.Label();
+            this.lblColonCostaricense = new System.Windows.Forms.Label();
+            this.lblNombreConvertidorMonedas = new System.Windows.Forms.Label();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.lblBalboas = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.cboOrigen = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblQuetzal
+            // 
+            this.lblQuetzal.AutoSize = true;
+            this.lblQuetzal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuetzal.Location = new System.Drawing.Point(8, 408);
+            this.lblQuetzal.Name = "lblQuetzal";
+            this.lblQuetzal.Size = new System.Drawing.Size(175, 16);
+            this.lblQuetzal.TabIndex = 25;
+            this.lblQuetzal.Text = "Quetzal Guatemalteco: ?";
+            // 
+            // lblPesoMexicano
+            // 
+            this.lblPesoMexicano.AutoSize = true;
+            this.lblPesoMexicano.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPesoMexicano.Location = new System.Drawing.Point(8, 383);
+            this.lblPesoMexicano.Name = "lblPesoMexicano";
+            this.lblPesoMexicano.Size = new System.Drawing.Size(129, 16);
+            this.lblPesoMexicano.TabIndex = 26;
+            this.lblPesoMexicano.Text = "Peso mexicano: ?";
+            // 
+            // lblSolPeruano
+            // 
+            this.lblSolPeruano.AutoSize = true;
+            this.lblSolPeruano.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSolPeruano.Location = new System.Drawing.Point(8, 357);
+            this.lblSolPeruano.Name = "lblSolPeruano";
+            this.lblSolPeruano.Size = new System.Drawing.Size(108, 16);
+            this.lblSolPeruano.TabIndex = 27;
+            this.lblSolPeruano.Text = "Sol Peruano: ?";
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEuro.Location = new System.Drawing.Point(8, 332);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(55, 16);
+            this.lblEuro.TabIndex = 28;
+            this.lblEuro.Text = "Euro: ?";
+            // 
+            // lblLempiraHondureño
+            // 
+            this.lblLempiraHondureño.AutoSize = true;
+            this.lblLempiraHondureño.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLempiraHondureño.Location = new System.Drawing.Point(8, 303);
+            this.lblLempiraHondureño.Name = "lblLempiraHondureño";
+            this.lblLempiraHondureño.Size = new System.Drawing.Size(159, 16);
+            this.lblLempiraHondureño.TabIndex = 29;
+            this.lblLempiraHondureño.Text = "Lempira Hondureño: ?";
+            // 
+            // lblDolarEstadounidense
+            // 
+            this.lblDolarEstadounidense.AutoSize = true;
+            this.lblDolarEstadounidense.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDolarEstadounidense.Location = new System.Drawing.Point(8, 275);
+            this.lblDolarEstadounidense.Name = "lblDolarEstadounidense";
+            this.lblDolarEstadounidense.Size = new System.Drawing.Size(176, 16);
+            this.lblDolarEstadounidense.TabIndex = 30;
+            this.lblDolarEstadounidense.Text = "Dólar estadounidense: ?";
+            // 
+            // lblDolarCanadiense
+            // 
+            this.lblDolarCanadiense.AutoSize = true;
+            this.lblDolarCanadiense.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDolarCanadiense.Location = new System.Drawing.Point(8, 247);
+            this.lblDolarCanadiense.Name = "lblDolarCanadiense";
+            this.lblDolarCanadiense.Size = new System.Drawing.Size(146, 16);
+            this.lblDolarCanadiense.TabIndex = 24;
+            this.lblDolarCanadiense.Text = "Dólar canadiense: ?";
+            // 
+            // lblCordoba
+            // 
+            this.lblCordoba.AutoSize = true;
+            this.lblCordoba.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCordoba.Location = new System.Drawing.Point(8, 217);
+            this.lblCordoba.Name = "lblCordoba";
+            this.lblCordoba.Size = new System.Drawing.Size(184, 16);
+            this.lblCordoba.TabIndex = 23;
+            this.lblCordoba.Text = "Córdoba Nicaraguense: ?";
+            // 
+            // lblColonCostaricense
+            // 
+            this.lblColonCostaricense.AutoSize = true;
+            this.lblColonCostaricense.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColonCostaricense.Location = new System.Drawing.Point(8, 188);
+            this.lblColonCostaricense.Name = "lblColonCostaricense";
+            this.lblColonCostaricense.Size = new System.Drawing.Size(158, 16);
+            this.lblColonCostaricense.TabIndex = 22;
+            this.lblColonCostaricense.Text = "Colon Costaricense: ?";
+            // 
+            // lblNombreConvertidorMonedas
+            // 
+            this.lblNombreConvertidorMonedas.AutoSize = true;
+            this.lblNombreConvertidorMonedas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreConvertidorMonedas.Location = new System.Drawing.Point(198, 29);
+            this.lblNombreConvertidorMonedas.Name = "lblNombreConvertidorMonedas";
+            this.lblNombreConvertidorMonedas.Size = new System.Drawing.Size(240, 24);
+            this.lblNombreConvertidorMonedas.TabIndex = 21;
+            this.lblNombreConvertidorMonedas.Text = "Convertidor de monedas";
+            // 
+            // lblOrigen
+            // 
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrigen.Location = new System.Drawing.Point(324, 91);
+            this.lblOrigen.Name = "lblOrigen";
+            this.lblOrigen.Size = new System.Drawing.Size(137, 16);
+            this.lblOrigen.TabIndex = 20;
+            this.lblOrigen.Text = "Moneda de origen:";
+            // 
+            // lblBalboas
+            // 
+            this.lblBalboas.AutoSize = true;
+            this.lblBalboas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBalboas.Location = new System.Drawing.Point(8, 158);
+            this.lblBalboas.Name = "lblBalboas";
+            this.lblBalboas.Size = new System.Drawing.Size(151, 16);
+            this.lblBalboas.TabIndex = 19;
+            this.lblBalboas.Text = "Balboa Panameño: ?";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(504, 63);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(139, 72);
+            this.btnConvertir.TabIndex = 18;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(158, 114);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(138, 20);
+            this.txtNum1.TabIndex = 17;
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(8, 118);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(144, 16);
+            this.lblNum1.TabIndex = 16;
+            this.lblNum1.Text = "Ingrese la cantidad:";
+            // 
+            // cboOrigen
+            // 
+            this.cboOrigen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOrigen.FormattingEnabled = true;
+            this.cboOrigen.Items.AddRange(new object[] {
+            "Balboa Panameño (PAB)",
+            "Colón Costaricense(CRC)",
+            "Córdoba Nicaraguense(NIO)",
+            "Dólar canadiense (CAD)",
+            "Dólar estadounidense (USD)",
+            "Lempira Hondureño (HNL)",
+            "Euro (ARS)",
+            "Sol Peruano(CLP)",
+            "Peso mexicano (MXN)",
+            "Quetzal Guatemalteco (GTQ)"});
+            this.cboOrigen.Location = new System.Drawing.Point(317, 113);
+            this.cboOrigen.Name = "cboOrigen";
+            this.cboOrigen.Size = new System.Drawing.Size(156, 21);
+            this.cboOrigen.TabIndex = 15;
+            // 
+            // FormConverMoned
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 439);
+            this.Controls.Add(this.lblQuetzal);
+            this.Controls.Add(this.lblPesoMexicano);
+            this.Controls.Add(this.lblSolPeruano);
+            this.Controls.Add(this.lblEuro);
+            this.Controls.Add(this.lblLempiraHondureño);
+            this.Controls.Add(this.lblDolarEstadounidense);
+            this.Controls.Add(this.lblDolarCanadiense);
+            this.Controls.Add(this.lblCordoba);
+            this.Controls.Add(this.lblColonCostaricense);
+            this.Controls.Add(this.lblNombreConvertidorMonedas);
+            this.Controls.Add(this.lblOrigen);
+            this.Controls.Add(this.lblBalboas);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.cboOrigen);
+            this.Name = "FormConverMoned";
+            this.Text = "Convertidor de Monedas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuetzal;
+        private System.Windows.Forms.Label lblPesoMexicano;
+        private System.Windows.Forms.Label lblSolPeruano;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.Label lblLempiraHondureño;
+        private System.Windows.Forms.Label lblDolarEstadounidense;
+        private System.Windows.Forms.Label lblDolarCanadiense;
+        private System.Windows.Forms.Label lblCordoba;
+        private System.Windows.Forms.Label lblColonCostaricense;
+        private System.Windows.Forms.Label lblNombreConvertidorMonedas;
+        private System.Windows.Forms.Label lblOrigen;
+        private System.Windows.Forms.Label lblBalboas;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.ComboBox cboOrigen;
+    }
+}
+
---
+++ Conversores/Conversores/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversores
+{
+    public partial class FormConverMoned : Form
+    {
+        public FormConverMoned()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /* Monedas 
+                 * Balboa (PAB)
+                   Colón (CRC)
+                   Córdoba (NIO)
+                   Dólar canadiense (CAD)
+                   Dólar estadounidense (USD)
+                   Lempira (HNL)
+                   Euros (EUR)
+                   Soles (PEN)
+                   Peso mexicano (MXN)
+                   Quetzal (GTQ)
+                  */
+
+
+        private double[][] conversiones = new double[][]
+        {
+            // 0 Balboas a
+            new double[] {
+                1, // Balboa
+                505.5433, // Colón costaricense
+                36.7899, // Córdoba
+                1.3831, // Dólar canadiense
+                1.0004, // Dólar estadounidense
+                26.4215, // Lempira
+                0.8557, // Euros 
+                3.5629, // Soles
+                18.7587, // Peso mexicano
+                7.6735 // Quetzal
+            },
+            // 1 colon cotaricense a
+            new double []  {
+                0.0020, // Balboa
+                1, // colon costaricense
+                0.0728, // Córdoba
+                0.0027, // Dólar canadiense
+                0.0020, // Dólar estadounidense
+                0.0523, // Lempira
+                0.0017, // Euros
+                0.0071, // Soles
+                0.0371, // peso mexicano
+                0.0152 // Quetzal
+            },
+
+            // 2 Cordoba a
+            new double []{
+                0.0272, // Balboa
+                13.7344, // Colón costaricense}
+                1, // Córdoba
+                0.0376, // Dólar canadiense
+                0.0272, // Dólar estadounidense
+                0.7178 ,// Lempira
+                0.0233, // Euros
+                0.0968, // Soles 
+                0.5096, // Peso mexicano
+                0.2085, // Quetzal
+
+            },
+            // 3 Dólar canadiense a
+            new double []{
+                0.7233, // Balboa
+                365.4687, // Colón costaricense}
+                26.5962, // Córdoba
+                1, // Dólar canadiense
+                0.7236, // Dólar estadounidense
+                19.1007 ,// Lempira
+                0.6183, // Euros 
+                2.5773, // Soles
+                13.5526, // Peso mexicano
+                5.5473, // Quetzal
+
+            },
+
+            // 4 Dólar estadounidense a
+            new double []{
+                0.9996, // Balboa
+                505.1326, // Colón costaricense}
+                36.7600, // Córdoba
+                1.3820, // Dólar canadiense
+                1, // Dólar estadounidense
+                26.4000 ,// Lempira
+                0.8542, // Euros
+                3.5619, // Soles
+                18.7435, // Peso mexicano
+                7.6634, // Quetzal
+
+            },
+
+            // 5 lempira a
+            new double []{
+                0.0379, // Balboa
+                19.1355, // Colón costaricense}
+                1.3925, // Córdoba
+                0.0524, // Dólar canadiense
+                0.0379, // Dólar estadounidense
+                1 ,// Lempira
+                0.0327, // Euros
+                0.1349, // Soles
+                0.7100, // Peso mexicano
+                0.2905, // Quetzal
+            },
+
+            // 6 Euros a
+            new double []{
+                1.1686, // Balboa
+                590.5000, // Colón costaricense}
+                43.0000, // Córdoba
+                1.6171, // Dólar canadiense
+                1.1706, // Dólar estadounidense
+                30.5900 ,// Lempira
+                1, // Euro
+                4.1654, // Soles
+                21.9247, // Peso mexicano
+                8.9630, // Quetzal
+
+            },
+            // 6 Soles a
+            new double []{
+                0.2806, // Balboa
+                141.8154, // Colón costaricense}
+                10.3203, // Córdoba
+                0.3882, // Dólar canadiense
+                0.2807, // Dólar estadounidense
+                7.4118 ,// Lempira
+                0.2402, // Euro
+                1, // Soles
+                5.2635, // Peso mexicano
+                2.1526, // Quetzal
+            },
+
+            // 7 Peso mexicano a
+            new double []{
+                0.0533, // Balboa
+                26.9498, // Colón costaricense}
+                1.9612, // Córdoba
+                0.0738, // Dólar canadiense
+                0.0534, // Dólar estadounidense
+                1.4085 ,// Lempira
+                0.0456, // Euro
+                0.1902, // Soles
+                1, // Peso mexicano
+                0.4091, // Quetzal
+            },
+
+            // 8 Quetzal  a
+            new double []{
+                0.1304, // Balboa
+                65.9148, // Colón costaricense}
+                4.7968, // Córdoba
+                0.1803, // Dólar canadiense
+                0.1305, // Dólar estadounidense
+                3.4449 ,// Lempira
+                0.1116, // Euro
+                0.4645, // Soles
+                2.4458, // Peso mexicano
+                1.4091, // Quetzal
+            }
+
+            // OJO DATOS TOMADOS DESDE Google Finance  17/08/2025
+        };
+
+        // Funcion para calcular las conversiones de las monedas
+        // No devuelve nada, solo actualiza los labels con las conversiones
+        // void para no devolver nada <--- Lo descubri al probar con funciones
+        public void conver(double cantidad, int origen)
+        {
+            // Da un recorrido por la matriz de conversiones
+            for (int i = 0; i <= conversiones.Length; i++)
+            {
+                // Si el indice de la matriz de monedas a convertir es igual al indice de la moneda de origen seleccionada
+                if (i == origen)
+                {
+                    // Actualiza los labels con las conversiones
+
+                    lblBalboas.Text = "Balboa Panameño = B/." + cantidad * conversiones[i][0];
+
+                    lblColonCostaricense.Text = "Colón costaricense = ₡" + cantidad * conversiones[i][1];
+
+                    lblCordoba.Text = "Córdoba Nicaragüense = C$" + cantidad * conversiones[i][2];
+
+                    lblDolarCanadiense.Text = "Dólar Canadiense = C$" + cantidad * conversiones[i][3];
+
+                    lblDolarEstadounidense.Text = "Dólar Estadounidense = $" + cantidad * conversiones[i][4];
+
+                    lblLempiraHondureño.Text = "Lempira Hondureño = L" + cantidad * conversiones[i][5];
+
+                    lblEuro.Text = "Euro = €" + cantidad * conversiones[i][6];
+
+                    lblSolPeruano.Text = "Soles Peruanos = S/." + cantidad * conversiones[i][7];
+
+                    lblPesoMexicano.Text = "Peso Mexicano = $" + cantidad * conversiones[i][8];
+
+                    lblQuetzal.Text = "Quetzal Guatemalteco = Q" + cantidad * conversiones[i][9];
+
+                }
+            }
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            double cantidad = 0;
+
+            int origen = cboOrigen.SelectedIndex; // Obtiene el indice de la moneda de origen seleccionada
+
+            // Validar si se ingresa la cantidad de dinero Ing.Cordero nos enseño sobre los messageBox
+            if (txtNum1.Text == "")
+            {
+                MessageBox.Show("Por favor, ingrese una cantidad de dinero válida.");
+                return;
+            }
+
+            cantidad = double.Parse(txtNum1.Text); // Convertir el texto a un número la cantidad de dinero
+
+            // Mostrar mensaje de error si no se selecciona la moneda de origen
+            if (cboOrigen.SelectedIndex == -1)
+            {
+                // Descubri que si no se seleciona retona -1 
+                MessageBox.Show("Por favor selecione la moneda de origen");
+                return;
+            }
+
+            // En los 10 casos posibles de monedas se llama a la funcion conver dependiendo el origen seleccionado
+            switch (cboOrigen.SelectedIndex)
+            {
+                case 0: // Balboa
+                    conver(cantidad, origen);
+                    break;
+                case 1: // Colón costaricense
+                    conver(cantidad, origen);
+                    break;
+                case 2: // Córdoba
+                    conver(cantidad, origen);
+                    break;
+                case 3: // Dólar canadiense
+                    conver(cantidad, origen);
+                    break;
+                case 4: // Dólar estadounidense
+                    conver(cantidad, origen);
+                    break;
+                case 5: // Lempira
+                    conver(cantidad, origen);
+                    break;
+                case 6: // Euros
+                    conver(cantidad, origen);
+                    break;
+                case 7: // Soles
+                    conver(cantidad, origen);
+                    break;
+                case 8: // Peso mexicano
+                    conver(cantidad, origen);
+                    break;
+                case 9: // Quetzal
+                    conver(cantidad, origen);
+                    break;
+
+            }
+        }
+    }
+}
